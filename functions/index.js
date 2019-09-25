c48c0832ec29be446ac03c99f24316894d80cb2b
rustiphyde <rustiphyde@gmail.com>
2019-09-25 02:03:20 -0500

    Add user signup route

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const config = require("./util/config");
 
 admin.initializeApp();
 
 const express = require('express');
 const app = express();
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+db = admin.firestore();
 
 // Function to fetch data from Firebase Firestore
 app.get('/sparks', (req, res) => {
@@ -47,6 +53,42 @@ app.post('/spark', (req, res) => {
         console.error(err);
     });
 });
+
+// Sign Up Route
+app.post("/signup", (req, res) => {
+    const newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      alias: req.body.alias,
+      clozang: ">" + req.body.alias.replace(/\s/g, "-").toLowerCase()
+    };
+  
+    // TODO validate data
+    db.doc(`/Users/${newUser.clozang}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return res
+            .status(400)
+            .json({ clozang: "This Clozang is already taken." });
+        } else {
+          return firebase
+            .auth()
+            .createUserWithEmailAndPassword(newUser.email, newUser.password);
+        }
+      })
+      .then(data => {
+        return data.user.getIdToken();
+      })
+      .then(token => {
+        return res.status(201).json({ token });
+      })
+      .catch(err=> {
+          console.error(err);
+          return res.status(500).json({ error: err.code });
+      })
+  });
 
 // Inform Firebase that 'app' is the container for all routes in application
 exports.api = functions.https.onRequest(app);

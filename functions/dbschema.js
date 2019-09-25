4102fd017d3f009614463420cb114033db74f22b
rustiphyde <rustiphyde@gmail.com>
2019-09-25 12:32:57 -0500

    Update dbschema to include a user object

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -1,7 +1,22 @@
 let db = {
+  users: [
+    {
+      userId: 'WniXVQLSLyM4q5HTKVuwWFGPyk2',
+      email: 'user@example.com',
+      alias: 'Generic Alias',
+      clozang: '>generic-alias',
+      createdAt: '2019-07-12T20:45:02.580Z',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/owlfuse-app.appspot.com/o/No-owlfuse-pic.png?alt=media',
+      bio: 'Hello, my name is Rusty, and I am the creator of Owlfuse.',
+      website: 'https://github.com/rustiphyde',
+      location: 'St. Louis, MO'
+    }
+
+  ],
   sparks: [
     {
-      clozang: "username",
+      alias: "Fake Username",
+      clozang: ">fake-username",
       body: "spark body",
       createdAt: "2019-07-12T17:19:04.980Z",
       heat: 6,

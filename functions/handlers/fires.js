5dfa812495cba977e2d75320c4dd882bc20eb916
rustiphyde <rustiphyde@gmail.com>
2019-09-25 22:10:55 -0500

    Add function to add a new stoke to a fire

--- functions/handlers/fires.js
+++ functions/handlers/fires.js
@@ -30,25 +30,65 @@ exports.getAllFires = (req, res) => {
 
 exports.getFire = (req, res) => {
   let fireData = {};
-  db.doc(`/Fires/${req.params.fireId}`).get()
-      .then((doc) => {
-          if(!doc.exists){
-              return res.status(404).json({ error: 'Fire not found'})
-          }
-          fireData = doc.data();
-          fireData.fireId = doc.id;
-          return db.collection('FireStokes').where('fireId', '==', req.params.fireId).get();
-      })
-      .then(data => {
-          fireData.stokes = [];
-          data.forEach(doc => {
-              fireData.stokes.push(doc.data())
-          });
-          return res.json(fireData);
-      })
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({ error: err.code});
+  db.doc(`/Fires/${req.params.fireId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Fire not found" });
+      }
+      fireData = doc.data();
+      fireData.fireId = doc.id;
+      return db
+        .collection("FireStokes")
+        .where("fireId", "==", req.params.fireId)
+        .get();
+    })
+    .then(data => {
+      fireData.stokes = [];
+      data.forEach(doc => {
+        fireData.stokes.push(doc.data());
       });
+      return res.json(fireData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
 };
 
+exports.addStokeToFire = (req, res) => {
+  if (req.body.body.trim() === "")
+    return res.status(400).json({ ember: "Field must not be empty" });
+
+  const newStoke = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    fireId: req.params.fireId,
+    alias: req.user.alias,
+    clozang: req.user.clozang,
+    userImage: req.user.imageUrl,
+    firstCandle: req.user.firstCandle
+  };
+
+  db.doc(`/Fires/${req.params.fireId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Fire has been extinguished" });
+      }
+      return doc.ref.update({
+        stokeCount: doc.data().stokeCount + 1,
+        heat: doc.data().heat + 1
+      });
+    })
+    .then(() => {
+      return db.collection("FireStokes").add(newStoke);
+    })
+    .then(() => {
+      res.json(newStoke);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Something went wrong" });
+    });
+};

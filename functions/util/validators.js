2efa27469756b2a3b5a612b4a6ce0c4787eebb2e
rustiphyde <rustiphyde@gmail.com>
2019-09-27 16:19:42 -0500

    Add alias to reduceUserDetails to allow updating it on front end

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -45,6 +45,7 @@ exports.reduceUserDetails = data => {
   let userDetails = {};
 
   //Make sure an empty string is not submitted to the database
+  if (!isEmpty(data.alias.trim())) userDetails.alias = data.alias;
   if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
   if (!isEmpty(data.website.trim())) {
     if (data.website.trim().substring(0, 4) !== "http") {

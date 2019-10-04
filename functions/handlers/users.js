619ac07e42dd66a6c60ea4e94dbd8d0183c52b88
rustiphyde <rustiphyde@gmail.com>
2019-10-04 09:38:06 -0500

    Update alias property

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -16,7 +16,7 @@ exports.signup = (req, res) => {
     email: req.body.email,
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
-    alias: req.body.alias.replace(/\s/g, "-"),
+    alias: req.body.alias,
     clozang: ">" + req.body.alias.replace(/\s/g, "-").toLowerCase()
   };
 

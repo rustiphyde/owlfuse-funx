0e273eb19e33662823ffbfa9470485ed96a2a1e8
rustiphyde <rustiphyde@gmail.com>
2019-10-08 11:23:11 -0500

    Update dbschema to reflect new usage of alias and userAlias

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -3,8 +3,7 @@ let db = {
     {
       userId: 'WniXVQLSLyM4q5HTKVuwWFGPyk2',
       email: 'user@example.com',
-      alias: 'Generic Alias',
-      clozang: '>generic-alias',
+      alias: 'Generic-Alias',
       createdAt: '2019-07-12T20:45:02.580Z',
       imageUrl: 'https://firebasestorage.googleapis.com/v0/b/owlfuse-app.appspot.com/o/No-owlfuse-pic.png?alt=media',
       bio: 'Hello, my name is Rusty, and I am the creator of Owlfuse.',
@@ -15,8 +14,7 @@ let db = {
   ],
   sparks: [
     {
-      alias: "Fake Username",
-      clozang: ">fake-username",
+      userAlias: "Fake-Username",
       body: "spark body",
       createdAt: "2019-07-12T17:19:04.980Z",
       heatCount: 6,
@@ -25,8 +23,7 @@ let db = {
   ],
   stokes: [
     {
-      alias: "Generic User",
-      clozang: ">generic-user",
+      userAlias: "Generic-User",
       sparkId: "9gDkPmho8hbRLf1J2IIB",
       body: "Stoke body goes here",
       createdAt: "2019-07-14T07:20:41:926Z"
@@ -35,7 +32,7 @@ let db = {
   Sizzles: [
     {
         recipient: "Rusty",
-        sender: "Other User",
+        sender: "Other-User",
         read: "true | false",
         sparkId: "9gDkPmho8hbRLf1J2IIB",
         type: "heat | stoke",
@@ -49,8 +46,7 @@ const userDetails = {
   credentials: {
       userId: "WniXVQLSLyM4q5HTKVuwWFG2Pyk2",
       email: "user@example.com",
-      alias: "Generic User",
-      clozang: ">generic-user",
+      alias: "Generic-User",
       createdAt: "2019-07-12T20:45:02.580Z",
       imageUrl: "https://firebasestorage.googleapis.com/v0/b/owlfuse.appspot.com/o/No-owlfuse-pic.png?alt=media",
       bio: "Hi, My name is Generic User",
@@ -59,11 +55,11 @@ const userDetails = {
   },
   heat: [
       {
-          alias: 'Rusty',
+          userAlias: 'Rusty',
           sparkId: "9gDkPmho8hbRLf1J2IIB"
       },
       {
-          alias: "User 2",
+          userAlias: "User 2",
           sparkId: "lSqXGaLI4e501rFodYy5"
       }
   ]

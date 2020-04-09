379a01d3ff1bdff18dc370ee5b35cade63c96e2f
rustiphyde <rustiphyde@gmail.com>
2020-04-08 21:50:45 -0500

    Fix syntax error

--- functions/index.js
+++ functions/index.js
@@ -110,7 +110,7 @@ app.get("/unsilence/:fuser", FBAuth, unsilenceFuser);
 app.post("/howl/:friend", FBAuth, postNewHowl);
 app.get("/howls", FBAuth, fetchUserHowls);
 app.get("/howl/:docKey", FBAuth, fetchSingleHowl);
-app.get("howlings/:docKey", FBAuth, fetchHowlings);
+app.get("/howlings/:docKey", FBAuth, fetchHowlings);
 app.delete("/howling/:howlId", FBAuth, silenceAHowling);
 app.delete("/howl/:docKey", FBAuth, silenceAHowl);
 app.post("/howling/edit/:howlId", FBAuth, editAHowling);
